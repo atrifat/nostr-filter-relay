10ffce4c98807437a71ef3cddc15d59b42144d32
Rif'at Ahdi R <10791791+atrifat@users.noreply.github.com>
2023-10-30 02:29:39 +0000

    refactor: change default behavior for in launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -8,12 +8,12 @@ bash -c 'while true;do cd /app/strfry/;/app/strfry/strfry relay > /dev/null 2>&1
 
 # Run nostr-monitoring-tool
 touch /app/logs/nmt.log
-# bash -c 'while true;do cd "/app/nostr-monitoring-tool/";NODE_ENV=development node "/app/nostr-monitoring-tool/src/index.mjs" >> /app/logs/nmt.log 2>&1;done' & true
-bash -c 'while true;do cd "/app/nostr-monitoring-tool/";node "/app/nostr-monitoring-tool/src/index.mjs" > /dev/null 2>&1;done' & true
+bash -c 'while true;do cd "/app/nostr-monitoring-tool/";node "/app/nostr-monitoring-tool/src/index.mjs" >> /app/logs/nmt.log 2>&1;done' & true
+# bash -c 'while true;do cd "/app/nostr-monitoring-tool/";node "/app/nostr-monitoring-tool/src/index.mjs" > /dev/null 2>&1;done' & true
 
 # Run nostr-filter relay as frontend relay proxy for strfry relay
 touch /app/logs/nf.log
 bash -c 'while true;do cd "/app/nostr-filter/";node "/app/nostr-filter/filter.js" >> /app/logs/nf.log 2>&1;done' & true
-# bash -c 'while true;do cd "/app/nostr-monitoring-tool/";node "/app/nostr-filter/filter.js" > /dev/null 2>&1;done' & true
+# bash -c 'while true;do cd "/app/nostr-filter/";node "/app/nostr-filter/filter.js" > /dev/null 2>&1;done' & true
 
 tail -F /app/logs/strfry.log /app/logs/nf.log /app/logs/nmt.log
